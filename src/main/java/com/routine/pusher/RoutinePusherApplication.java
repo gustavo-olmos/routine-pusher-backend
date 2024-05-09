ee70e3d2e8622dd9e84649094885cd7405dc01ba
Gustavo <guolmos06@gmail.com>
2024-05-09 00:39:25 -0300

    Inicialização do repositório

---
+++ src/main/java/com/routine/pusher/RoutinePusherApplication.java
@@ -0,0 +1,13 @@
+package com.routine.pusher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutinePusherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutinePusherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/routine/pusher/RoutinePusherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routine.pusher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutinePusherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
